adfc5daa429b59a8222790f8b682b0a477aa9354
cukidev <paloma.zepeda@usach.cl>
2024-12-09 18:04:39 -0300

    [feat] Añadido detalle del carrito

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import ListaProovView from './views/ListaProovView';
 import ListProductsView from './views/ListProductsView';
 import InventoryView from './views/InventoryView';
 import SidebarCart from './components/SidebarCart';
-import  ProductForm  from './components/ProductForm';
+import ProductForm from './components/ProductForm';
 import ProveedorForm from './components/ProveedorForm';
 import FrutosSecos from './views/FrutosSecos';
 import Semillas from './views/Semillas';
@@ -20,11 +20,12 @@ import CerealesIntegrales from './views/CerealesIntegrales';
 import Legumbres from './views/Legumbres';
 import Envasados from './views/Envasados';
 import AboutUs from './views/AboutUs';
-import Profile from './views/Profile'
+import Profile from './views/Profile';
 import Orders from './views/Orders';
 import AddStock from './views/addStock';
+import CartSummary from './components/CartSummary';
 import { Routes, Route } from 'react-router-dom';
-
+import { CartProvider } from './context/CartContext';
 
 function App() {
   const [isCartOpen, setIsCartOpen] = useState(false);
@@ -33,37 +34,40 @@ function App() {
   const handleCartClose = () => setIsCartOpen(false);
 
   return (
-    <div className="App">
-      <Header onCartClick={handleCartOpen} />
-      <Navbar /> {}
+    <CartProvider>
+      <div className="App">
+        <Header onCartClick={handleCartOpen} />
+        <Navbar />
 
-      <div className="app-content">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/productos" element={<ProductView />} />
-          <Route path="/sustentabilidad" element={<ReuseView />} />
-          <Route path="/frutos-secos" element={<FrutosSecos />} />
-          <Route path="/semillas" element={<Semillas />} />
-          <Route path="/cereales-integrales" element={<CerealesIntegrales />} />
-          <Route path="/legumbres" element={<Legumbres />} />
-          <Route path="/envasados" element={<Envasados />} />
-          <Route path="/nosotros" element={<AboutUs />} />
-          <Route path="/inventario" element={<InventoryView />} />
-          <Route path="/listaProov" element={<ListaProovView />} />
-          <Route path="/listaProduct" element={<ListProductsView />} />
-          <Route path="/productForm" element={<ProductForm />} />
-          <Route path="/proveedorForm" element={<ProveedorForm />} />
-          <Route path="/login" element={<LoginView />} />
-          <Route path="/registro" element={<RegisterView />} />
-          <Route path="/perfil" element={<Profile />} />
-          <Route path="/pedidos" element={<Orders />} />
-          <Route path="/addStock" element={<AddStock />} />
-        </Routes>
+        <div className="app-content">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/productos" element={<ProductView />} />
+            <Route path="/sustentabilidad" element={<ReuseView />} />
+            <Route path="/frutos-secos" element={<FrutosSecos />} />
+            <Route path="/semillas" element={<Semillas />} />
+            <Route path="/cereales-integrales" element={<CerealesIntegrales />} />
+            <Route path="/legumbres" element={<Legumbres />} />
+            <Route path="/envasados" element={<Envasados />} />
+            <Route path="/nosotros" element={<AboutUs />} />
+            <Route path="/inventario" element={<InventoryView />} />
+            <Route path="/listaProov" element={<ListaProovView />} />
+            <Route path="/listaProduct" element={<ListProductsView />} />
+            <Route path="/productForm" element={<ProductForm />} />
+            <Route path="/proveedorForm" element={<ProveedorForm />} />
+            <Route path="/login" element={<LoginView />} />
+            <Route path="/registro" element={<RegisterView />} />
+            <Route path="/perfil" element={<Profile />} />
+            <Route path="/pedidos" element={<Orders />} />
+            <Route path="/addStock" element={<AddStock />} />
+            <Route path="/resumen" element={<CartSummary />} />
+          </Routes>
+        </div>
+
+        <Footer />
+        <SidebarCart isOpen={isCartOpen} onClose={handleCartClose} />
       </div>
-
-      <Footer />
-      <SidebarCart isOpen={isCartOpen} onClose={handleCartClose} />
-    </div>
+    </CartProvider>
   );
 }
 
