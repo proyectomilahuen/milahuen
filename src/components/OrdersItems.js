0c7e9d40397437275eccf34c81515956c3a9c97e
FranciscaCMG <francisca.marquez@usach.cl>
2024-12-19 00:09:33 -0300

    [Fix] diseño lista ordenes

--- src/components/OrdersItems.js
+++ src/components/OrdersItems.js
@@ -4,158 +4,167 @@ import { Box, Collapse, IconButton, Table, TableBody, TableCell, TableContainer,
 import { KeyboardArrowDown, KeyboardArrowUp } from '@mui/icons-material';
 import { makeStyles } from '@mui/styles';
 import axios from 'axios';
+import '../styles/OrdersItems.css';
 
 const useRowStyles = makeStyles({
-  root: {
-    '& > *': {
-      borderBottom: 'unset',
+    root: {
+        '& > *': {
+            borderBottom: 'unset',
+        },
     },
-  },
 });
 
 function Row(props) {
-  const { row, history } = props;
-  const [open, setOpen] = React.useState(false);
-  const classes = useRowStyles();
+    const { row, history, isSecondRow } = props;
+    const [open, setOpen] = React.useState(false);
+    const classes = useRowStyles();
 
-  console.log(history);
-
-  return (
-    <React.Fragment>
-      <TableRow className={classes.root}>
-        <TableCell>
-          <IconButton aria-label="expand row" size="small" onClick={() => setOpen(!open)}>
-            {open ? <KeyboardArrowUp /> : <KeyboardArrowDown />}
-          </IconButton>
-        </TableCell>
-        <TableCell component="th" scope="row">
-          {row.name}
-        </TableCell>
-        <TableCell align="left">{row.order_date}</TableCell>
-        <TableCell align="left">{row.status}</TableCell>
-        <TableCell align="left">{row.total_price}</TableCell>
-      </TableRow>
-      <TableRow>
-        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
-          <Collapse in={open} timeout="auto" unmountOnExit>
-            <Box margin={1}>
-              <Typography variant="h6" gutterBottom component="div">
-                Detalles adicionales
-              </Typography>
-              <Typography variant="body2">Dirección: {row.address}</Typography>
-              <Typography variant="body2">Teléfono: {row.contact_number}</Typography>
-              <Typography variant="body2">Correo: {row.email}</Typography>
-              <Typography variant="body2" gutterBottom>Historial de artículos:</Typography>
-              <Table size="small" aria-label="purchases">
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Producto</TableCell>
-                    <TableCell align="left">Cantidad</TableCell>
-                    <TableCell align="left">Precio</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {history.map((item) => (
-                    <TableRow key={item.id}>
-                      <TableCell component="th" scope="row">
-                        Producto {item.product}
-                      </TableCell>
-                      <TableCell align="left">{item.quantity}</TableCell>
-                      <TableCell align="left">{item.price_at_purchase}</TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </Box>
-          </Collapse>
-        </TableCell>
-      </TableRow>
-    </React.Fragment>
-  );
+    return (
+        <React.Fragment>
+            <TableRow className={`${classes.root} ${isSecondRow ? 'second-row' : 'first-row'}`}>
+                <TableCell>
+                    <IconButton aria-label="expand row" size="small" onClick={() => setOpen(!open)}>
+                        {open ? <KeyboardArrowUp /> : <KeyboardArrowDown />}
+                    </IconButton>
+                </TableCell>
+                <TableCell component="th" scope="row">
+                    {row.name}
+                </TableCell>
+                <TableCell align="left">
+                    {new Date(row.order_date).toLocaleDateString('es-ES')}
+                </TableCell>
+                <TableCell align="left">{row.status}</TableCell>
+                <TableCell align="left">{row.total_price}</TableCell>
+            </TableRow>
+            <TableRow>
+                <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+                    <Collapse in={open} timeout="auto" unmountOnExit>
+                        <Box margin={1}>
+                            <Typography variant="h6" gutterBottom component="div">
+                                Detalles adicionales
+                            </Typography>
+                            <Typography variant="body2">Dirección: {row.address}</Typography>
+                            <Typography variant="body2">Teléfono: {row.contact_number}</Typography>
+                            <Typography variant="body2">Correo: {row.email}</Typography>
+                            <Typography variant="body2" gutterBottom>Historial de artículos:</Typography>
+                            <Table size="small" aria-label="purchases">
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell>Producto</TableCell>
+                                        <TableCell align="left">Cantidad</TableCell>
+                                        <TableCell align="left">Precio</TableCell>
+                                    </TableRow>
+                                </TableHead>
+                                <TableBody>
+                                    {history.map((item) => (
+                                        <TableRow key={item.id}>
+                                            <TableCell component="th" scope="row">
+                                                Producto {item.product}
+                                            </TableCell>
+                                            <TableCell align="left">{item.quantity}</TableCell>
+                                            <TableCell align="left">{item.price_at_purchase}</TableCell>
+                                        </TableRow>
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </Box>
+                    </Collapse>
+                </TableCell>
+            </TableRow>
+        </React.Fragment>
+    );
 }
 
+
+
 Row.propTypes = {
-  row: PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    name: PropTypes.string.isRequired,
-    address: PropTypes.string.isRequired,
-    contact_number: PropTypes.string.isRequired,
-    email: PropTypes.string.isRequired,
-    status: PropTypes.string.isRequired,
-    order_date: PropTypes.string.isRequired,
-    total_price: PropTypes.number.isRequired,
-  }).isRequired,
-  history: PropTypes.array.isRequired,
+    row: PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        name: PropTypes.string.isRequired,
+        address: PropTypes.string.isRequired,
+        contact_number: PropTypes.string.isRequired,
+        email: PropTypes.string.isRequired,
+        status: PropTypes.string.isRequired,
+        order_date: PropTypes.string.isRequired,
+        total_price: PropTypes.number.isRequired,
+    }).isRequired,
+    history: PropTypes.array.isRequired,
 };
 
 export default function OrdersItems() {
-  const [rows, setRows] = useState([]);
-  const [errorMessage, setErrorMessage] = useState('');
-  const [historyData, setHistoryData] = useState({});
+    const [rows, setRows] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('');
+    const [historyData, setHistoryData] = useState({});
 
-  useEffect(() => {
-    const fetchOrders = async () => {
-      try {
-        const token = localStorage.getItem('token');
-        if (!token) {
-          setErrorMessage('No estás autenticado. Por favor, inicia sesión.');
-          return;
-        }
+    useEffect(() => {
+        const fetchOrders = async () => {
+            try {
+                const token = localStorage.getItem('token');
+                if (!token) {
+                    setErrorMessage('No estás autenticado. Por favor, inicia sesión.');
+                    return;
+                }
 
-        const ordersResponse = await axios.get('https://emporio-milahuen.onrender.com/api/ordenes/', {
-          headers: {
-            Authorization: `Token ${token}`,
-          },
-        });
+                const ordersResponse = await axios.get('https://emporio-milahuen.onrender.com/api/ordenes/', {
+                    headers: {
+                        Authorization: `Token ${token}`,
+                    },
+                });
 
-        const ordersData = ordersResponse.data;
+                const ordersData = ordersResponse.data;
 
-        const historyPromises = ordersData.map(async (order) => {
-          const historyResponse = await axios.get(`https://emporio-milahuen.onrender.com/api/orden_items/order/${order.id}/`);
-          return { orderId: order.id, history: historyResponse.data };
-        });
+                const historyPromises = ordersData.map(async (order) => {
+                    const historyResponse = await axios.get(`https://emporio-milahuen.onrender.com/api/orden_items/order/${order.id}/`);
+                    return { orderId: order.id, history: historyResponse.data };
+                });
 
-        const historyResponses = await Promise.all(historyPromises);
+                const historyResponses = await Promise.all(historyPromises);
 
-        const historyMap = historyResponses.reduce((acc, { orderId, history }) => {
-          acc[orderId] = history;
-          return acc;
-        }, {});
+                const historyMap = historyResponses.reduce((acc, { orderId, history }) => {
+                    acc[orderId] = history;
+                    return acc;
+                }, {});
 
-        setRows(ordersData);
-        setHistoryData(historyMap);
-        setErrorMessage(''); 
-      } catch (error) {
-        console.error('Error al obtener los productos:', error);
-        setErrorMessage('Error al cargar la lista de productos.');
-      }
-    };
+                setRows(ordersData);
+                setHistoryData(historyMap);
+                setErrorMessage('');
+            } catch (error) {
+                console.error('Error al obtener los productos:', error);
+                setErrorMessage('Error al cargar la lista de productos.');
+            }
+        };
 
-    fetchOrders();
-  }, []);
+        fetchOrders();
+    }, []);
 
-  return (
-    <Box>
-      {errorMessage && <Alert severity="error">{errorMessage}</Alert>}
+    return (
+        <Box>
+            {errorMessage && <Alert severity="error">{errorMessage}</Alert>}
 
-      <TableContainer component={Paper}>
-        <Table aria-label="collapsible table">
-          <TableHead>
-            <TableRow>
-              <TableCell />
-              <TableCell>Nombre</TableCell>
-              <TableCell align="left">Fecha de Orden</TableCell>
-              <TableCell align="left">Estado</TableCell>
-              <TableCell align="left">Precio Total</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.map((row) => (
-              <Row key={row.id} row={row} history={historyData[row.id] || []} />
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </Box>
-  );
+            <TableContainer component={Paper}>
+                <Table aria-label="collapsible table">
+                    <TableHead className="table-head">
+                        <TableRow>
+                            <TableCell />
+                            <TableCell className='tablecell'>Nombre</TableCell>
+                            <TableCell align="left">Fecha de Orden</TableCell>
+                            <TableCell align="left">Estado</TableCell>
+                            <TableCell align="left">Precio Total</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {rows.map((row, index) => (
+                            <Row
+                                key={row.id}
+                                row={row}
+                                history={historyData[row.id] || []}
+                                isSecondRow={index % 2 !== 0}
+                            />
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </Box>
+    );
 }
+
